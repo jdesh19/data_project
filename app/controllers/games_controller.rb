a5fc2b94b7b1453e7d4d03615d163fcbe29f6ae7
jdesh19 <jarondesharnais@gmail.com>
2024-07-02 20:00:07 -0500

    Added if statment to index view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,10 @@
 class GamesController < ApplicationController
+
   def index
     @q = Game.ransack(params[:q])
     @games = @q.result(distinct: true).includes(:genre, :platform, :publisher).page(params[:page]).per(10)
   end
+
   def show
     @game = Game.find(params[:id])
   end
